165a480ef76ad8b8a167d38cc4c45931d8515fef
phamLinh98 <linhthusinh98@gmail.com>
2025-05-03 17:38:48 +0900

    Refactor API stack to remove unused code and add CORS headers

--- cdk/lib/api-stack.ts
+++ cdk/lib/api-stack.ts
@@ -22,7 +22,7 @@ export class ApiStack extends cdk.Stack {
     //TODO: create a new lambda function name create-presigned-url-uploading-lambda get source from src/rebuild/create-preurl
     const createPresignedUrlLambda = createNewLambdaFunction(this, 'CreatePresignedUrlLambda', 'create-presigned-url-uploading-lambda', './src/rebuild/create-preurl', "create-preurl-s3-update-status-uploading-lambda.mjs", 'create-preurl-s3-update-status-uploading-lambda.handler');
 
-    //create role for lambda function createPresignedUrlLambda to access s3 and dynamoDB
+    //TODO: create role for lambda function createPresignedUrlLambda to access s3 and dynamoDB
     const listTableInDynamoDB = [usersTable, uploadCsvTable];
     grantServiceListServiceReadWriteAnService(listTableInDynamoDB, 'grantReadWriteData', createPresignedUrlLambda);
 
@@ -33,13 +33,15 @@ export class ApiStack extends cdk.Stack {
 
     //TODO: create a new lambda function name get-batchid-update-status-to-uploaded get source from src/rebuild/get-batchid-uploaded
     const getBatchIdUpdateStatusToUploadedLambda = createNewLambdaFunction(this, 'GetBatchIdUpdateStatusToUploadedLambda', 'get-batchid-update-status-to-uploaded', './src/rebuild/get-batchid-uploaded', "get-batchid-update-status-to-uploaded.mjs", 'get-batchid-update-status-to-uploaded.handler');
-    // getBatchIdUpdateStatusToUploadedLambda can read and write dynamoDb
+
+    //TODO: getBatchIdUpdateStatusToUploadedLambda can read and write dynamoDb
     grantServiceListServiceReadWriteAnService(listTableInDynamoDB, 'grantReadWriteData', getBatchIdUpdateStatusToUploadedLambda);
 
     //TODO: create a new lambda function name get-csv-read-detail-update-inprocessing-lambda get source from src/rebuild/get-batchid-uploaded
     const getCsvReadDetailUpdateInProcessingLambda = createNewLambdaFunction(this, 'GetCsvReadDetailUpdateInProcessingLambda', 'get-csv-read-detail-update-inprocessing-lambda', './src/rebuild/get-csv-read-detail', "get-csv-read-detail-update-inprocessing-lambda.mjs", 'get-csv-read-detail-update-inprocessing-lambda.handler');
     grantServiceListServiceReadWriteAnService(listTableInDynamoDB, 'grantReadWriteData', getCsvReadDetailUpdateInProcessingLambda);
-    // Add policy to lambda function to access SQS
+   
+    // Add policy IAM to Lambda function to access SQS
     const listSqsRoleInIAM = ['sqs:SendMessage', 'sqs:ReceiveMessage', 'sqs:DeleteMessage', 'sqs:GetQueueAttributes', 'sqs:ListQueues'];
     const sqsArn = [queueSQS.queueArn];
     const sqsPolicy = settingNewPolicy(listSqsRoleInIAM, sqsArn);
@@ -63,35 +65,32 @@ export class ApiStack extends cdk.Stack {
 
 
     //TODO: adding connect role to lambda function to access S3 
-
     grantServiceAnServiceReadWriteAListService(bucketCsvS3, 'grantReadWrite', listLambdaFunction);
 
     // Add policy to lambda function to access S3
     const s3Policy = settingNewPolicy(['s3:PutObject', 's3:GetObject'], [bucketCsvS3.bucketArn + '/*']);
     grantServiceListServiceReadWriteAnService(listLambdaFunction, 'addToRolePolicy', s3Policy);
 
-    // //TODO: khi sqs queueSQS có message trong queue thì sẽ trigger lambda getCsvReadDetailUpdateInProcessingLambda
-    // //SQS trigger Lambda getCsvReadDetailUpdateInProcessingLambda
+    //TODO: SQS trigger Lambda getCsvReadDetailUpdateInProcessingLambda
     const queueSQSTrigger = settingSqsBatchSizeCurrentcy(queueSQS, 10, 5);
     getCsvReadDetailUpdateInProcessingLambda.addEventSource(queueSQSTrigger);
 
-    // // TODO: thiết lập, khi có 1 file csv upload lên s3 bucketCsvS3 thì sẽ trigger lambda getBatchIdUpdateStatusToUploadedLambda
-    // //S3 trigger Lambda getBatchIdUpdateStatusToUploadedLambda
+    //TODO: Lambda getBatchIdUpdateStatusToUploadedLambda triggered by S3 bucketCsvS3 when a new file is uploaded
     const bucketCsvS3Notification = settingS3Notification(bucketCsvS3, '.csv');
     getBatchIdUpdateStatusToUploadedLambda.addEventSource(bucketCsvS3Notification);
 
-    // // Create an API Gateway
-    const apiGatewaySetting = settingApiGatewayRoleCors(this, 'LinhClassApiGateway');
+    //Create an API Gateway
+    const apiName = settingApiGatewayRoleCors(this, 'LinhClassApiGateway');
 
     // GET get-url endpoint calling createPresignedUrlLambda
     const getUrlIntegration = setupApiGatewayForLambdaFn(createPresignedUrlLambda);
-    apiGatewaySetting.root.addResource('get-url').addMethod('GET', getUrlIntegration);
+    apiName.root.addResource('get-url').addMethod('GET', getUrlIntegration);
 
     // GET get-status endpoint calling getStatusFromDynamoDBLambda
     const getStatusIntegration = setupApiGatewayForLambdaFn(getStatusFromDynamoDBLambda);
-    apiGatewaySetting.root.addResource('get-status').addMethod('GET', getStatusIntegration);
+    apiName.root.addResource('get-status').addMethod('GET', getStatusIntegration);
 
-    //TODO setting for secretManager for all lambda function
+    //TODO setting secretManager for all lambda function using
     grantServiceAnServiceReadWriteAListService(mySecret, 'grantRead', listLambdaFunction);
   }
 }
--- cdk/lib/custom-constracts/csv-upload-resources.ts
+++ cdk/lib/custom-constracts/csv-upload-resources.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import { configCors, configCorsApiGateway } from '../../utils/cors';
 import { Function as LambdaFunction } from 'aws-cdk-lib/aws-lambda';
 
+//TODO: Create New Bucket S3
 export const createNewBucketS3 = (scope: Construct, idBucket: any, bucketName: string) => {
       return new cdk.aws_s3.Bucket(scope, idBucket, {
             bucketName: bucketName,
@@ -12,12 +13,23 @@ export const createNewBucketS3 = (scope: Construct, idBucket: any, bucketName: s
       });
 };
 
+//TODO: Create New SQS queue 
 export const createNewSQS = (scope: Construct, idQueue: any, queueName: any, maxTime: any) => {
       return new cdk.aws_sqs.Queue(scope, idQueue, {
             queueName: queueName,
             retentionPeriod: cdk.Duration.days(maxTime),
       });
 }
+
+//TODO: Setting SQS Batch Size and Max Concurrency
+export const settingSqsBatchSizeCurrentcy = (queueName: any, batchSize: any, maxConcurrency: any) => {
+      return new cdk.aws_lambda_event_sources.SqsEventSource(queueName, {
+            batchSize: batchSize,
+            maxConcurrency: maxConcurrency
+      });
+}
+
+//TODO; Setting Policy for Service
 export const settingNewPolicy = (actionList: any, queueArn: any) => {
       return new cdk.aws_iam.PolicyStatement({
             actions: actionList,
@@ -25,6 +37,7 @@ export const settingNewPolicy = (actionList: any, queueArn: any) => {
       });
 }
 
+//TODO: Create new table in DynamoDB
 export const createNewTableDynamoDB = (scope: Construct, idTable: any, tableName: string) => {
       return new cdk.aws_dynamodb.Table(scope, idTable, {
             tableName: tableName,
@@ -33,6 +46,7 @@ export const createNewTableDynamoDB = (scope: Construct, idTable: any, tableName
       });
 }
 
+//TODO: Create new Lambda function
 export const createNewLambdaFunction = (scope: Construct, idLambda: any, lambdaName: any, path: any, excludeFunction: any, lambdaHander: any) => {
       return new cdk.aws_lambda.Function(scope, idLambda, {
             functionName: lambdaName,
@@ -44,25 +58,21 @@ export const createNewLambdaFunction = (scope: Construct, idLambda: any, lambdaN
       });
 }
 
+//TODO: Setting A List Service Can Read/Write A Service
 export const grantServiceListServiceReadWriteAnService = (listService: any, policy: string, service: any) => {
       return listService.forEach((list: any) => {
             list[policy](service);
       });
 }
 
+//TODO: Setting A Service Can Read/Write A List Service
 export const grantServiceAnServiceReadWriteAListService = (service: any, policy: any, ListService: any) => {
       return ListService.forEach((list: any) => {
             service[policy](list);
       });
 }
 
-export const settingSqsBatchSizeCurrentcy = (queueName: any, batchSize: any, maxConcurrency: any) => {
-      return new cdk.aws_lambda_event_sources.SqsEventSource(queueName, {
-            batchSize: batchSize,
-            maxConcurrency: maxConcurrency
-      });
-}
-
+//TODO; Setting S3 Notification When New file add to S3
 export const settingS3Notification = (bucketName:any, filterFile:any) => {
       return new cdk.aws_lambda_event_sources.S3EventSource(bucketName, {
             events: [cdk.aws_s3.EventType.OBJECT_CREATED],
@@ -70,6 +80,7 @@ export const settingS3Notification = (bucketName:any, filterFile:any) => {
       });
 }
 
+//TODO: Setting CORS for API Gateway
 export const settingApiGatewayRoleCors = (scope:any, apiGatewayName:any, ) => {
      return new cdk.aws_apigateway.RestApi(scope, apiGatewayName, {
       restApiName: apiGatewayName,
@@ -78,6 +89,7 @@ export const settingApiGatewayRoleCors = (scope:any, apiGatewayName:any, ) => {
     }); 
 }
 
+//TODO: Setup API Gateway for Lambda Function
 export const setupApiGatewayForLambdaFn = (lambdaFunc:any) => {
       return new cdk.aws_apigateway.LambdaIntegration(lambdaFunc);
 }
