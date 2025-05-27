85d3f2ba4d934f230b06e1a340eba3b583befc4a
phamLinh98 <linhthusinh98@gmail.com>
2025-05-27 21:25:29 +0900

    Refactor build process scripts to improve modularity and readability

---
+++ scripts/build-script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Starting script/build.sh"
+
+# Install Pharse
+echo "Running install pharse"
+ls -la
+
+echo "Installing dependencies"
+npm install
+npm install --save-dev @types/node
+cd cdk && npm install && npm install --save-dev @types/node && cd ..
+
+echo "Check Node.js version"
+node -v
+
+echo "Check npm version"
+npm -v
+
+echo "Check npx version"
+npx -v
+
+echo "Check aws cli version"
+aws --version
+
+# Prebuild Pharse
+echo "Running pre-build pharse"
+npx tsc
+
+# Build Pharse
+echo "Running build pharse"
+
+echo "Showing directory structure"
+ls -la
+
+echo "Running cdk deploy"
+npm run deploy
