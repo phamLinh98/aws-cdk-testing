522a2a7b18add235b8f74462bdbfc268d66f43db
phamLinh98 <linhthusinh98@gmail.com>
2025-05-27 22:43:07 +0900

    Fix missing closing parenthesis in logging statement

--- src/lambda/create-preurl-s3-update-status-uploading-lambda/index.ts
+++ src/lambda/create-preurl-s3-update-status-uploading-lambda/index.ts
@@ -11,7 +11,6 @@ import { getSecretOfKey } from "../get-secret-key-from-manager";
 
 export const handler = async (event: any) => {
   try {
-    console.log("event >>>", event
     // Get the bucket name and table name from Secrets Manager
     const bucketName = (await getSecretOfKey("bucketCsvName")) as any;
     console.log("bucketName >>>", bucketName);
